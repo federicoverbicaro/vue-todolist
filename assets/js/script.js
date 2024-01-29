9e1601854c769869b86c132f415dae9d59ab4380
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-29 16:10:51 +0100

    feat:prima parte

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            todo:[
+               'lezione sul DOM',
+                'lezione if-else',
+                'lezione ciclo for'
+            ],
+            nuovoTodo: [""]
+            
+        }
+    },
+
+    methods: {
+
+        
+
+    }
+
+}).mount('#app')
